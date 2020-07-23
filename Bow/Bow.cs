538bc2cc4102562287385609cc871aa11d197849
towergamesunion <towergamesunion@gmail.com>
2020-07-23 13:25:28 +0300

    Start.

---
+++ Bow/Bow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class Bow : MonoBehaviour
+{
+    [SerializeField] private GameObject Arrow;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Bow/BowInput.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowInput : MonoBehaviour
+{
+    public float LaunchForce;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void CalcForceAtDrag()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                
+            }
+        }
+    }
+    
+}
